bbb80b56bc5b750ec4d4593f2ce9b5ad152388ab
Nakib00 <2022142@iub.edu.bd>
2025-09-19 08:00:46 +0600

    fsadf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -149,7 +149,7 @@ const Navbar = () => {
                     <div className="flex items-center space-x-4">
                         <div className="hidden md:flex items-center space-x-4">
                             <button onClick={toggleTheme} className="p-2 rounded-lg bg-gray-700"><Moon className="w-5 h-5 text-white" /></button>
-                            <a href="https://zantechbd.com/"><button className="bg-zan-red text-white px-6 py-2 rounded-full">Store</button></a>
+                            <a href="https://store.zantechbd.com"><button className="bg-zan-red text-white px-6 py-2 rounded-full">Store</button></a>
                         </div>
                         <button onClick={() => setMobileMenuOpen(!isMobileMenuOpen)} className="md:hidden p-2 rounded-lg text-white">
                             {isMobileMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
